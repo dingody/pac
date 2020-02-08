17d42ced5e5b592e7bd5ffbdf6ab6ca6683ec95e
dingody <dingody@users.noreply.github.com>
2020-02-08 11:48:01 +0800

    Update c.py

--- c.py
+++ c.py
@@ -1,1 +1,26 @@
+from string import Template
+switchy_template = """[SwitchyOmega Conditions]
+@with result
 
+$domain
+
+* +direct
+"""
+domain_template = """*$domain* +proxy
+"""
+
+st = Template(switchy_template)
+dt = Template(domain_template)
+
+f = open("d.txt")
+domain = ""
+for i in f.readlines():
+  templine = i
+  if templine.find("DOMAIN-KEYWORD")!=-1 or templine.find("DOMAIN-KEYWORD")!=-1:
+    domain = domain + dt.substitute(domain=templine.split(",")[1])
+f.close()
+
+switchy_pac = st.substitute(domain=domain)
+f = open('d.pac', 'w')
+f.write(switchy_pac)
+f.close()
