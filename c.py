e89ab6f3a4e3dae52cf9f4185551e379d0d17aa6
dingody <dingody@users.noreply.github.com>
2020-06-10 10:30:49 +0800

    Update c.py

--- c.py
+++ c.py
@@ -22,9 +22,12 @@ domain = ""
 domain_cb = ""
 for i in f.readlines():
   templine = i
-  if templine.find("DOMAIN-KEYWORD")!=-1 or templine.find("DOMAIN-SUFFIX")!=-1:
+  if templine.find("DOMAIN-KEYWORD")!=-1:
     domain = domain + dt.substitute(domain=templine.split(",")[1])
     domain_cb = domain_cb + dt_cb.substitute(domain_cb=templine.split(",")[1])
+  if templine.find("DOMAIN-SUFFIX")!=-1:
+    domain = domain + dt.substitute(domain="."+templine.split(",")[1])
+    domain_cb = domain_cb + dt_cb.substitute(domain_cb="."+templine.split(",")[1])
 f.close()
 
 switchy_pac = st.substitute(domain=domain)
