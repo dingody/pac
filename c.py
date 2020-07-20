7230808a02c9be80c82f1485ffe396d71c2547c0
dingody <dingody@users.noreply.github.com>
2020-07-21 00:21:39 +0800

    Update c.py

--- c.py
+++ c.py
@@ -7,15 +7,16 @@ $domain
 * +direct
 """
 
-domain_template = """*$domain* +proxy
+v2rayng_template = """
+$domain
 """
 
-domain_template_cb = """*$domain_cb* +proxy-cb
+domain_template = """*$domain* +proxy
 """
 
 st = Template(switchy_template)
 dt = Template(domain_template)
-dt_cb = Template(domain_template_cb)
+dt_cb = Template(v2rayng_template)
 
 f = open("d.conf")
 domain = ""
@@ -24,10 +25,12 @@ for i in f.readlines():
   templine = i
   if templine.find("DOMAIN-KEYWORD")!=-1:
     domain = domain + dt.substitute(domain=templine.split(",")[1])
-    domain_cb = domain_cb + dt_cb.substitute(domain_cb=templine.split(",")[1])
+    domain_cb = domain_cb + "geosite:"+templine.split(",")[1]+","
   if templine.find("DOMAIN-SUFFIX")!=-1:
     domain = domain + dt.substitute(domain="."+templine.split(",")[1])
-    domain_cb = domain_cb + dt_cb.substitute(domain_cb="."+templine.split(",")[1])
+    domain_cb = domain_cb + "domain:"+templine.split(",")[1]+","
+  if templine.find("IP-CIDR")!=-1:
+    domain_cb = domain_cb + templine.split(",")[1]+","
 f.close()
 
 switchy_pac = st.substitute(domain=domain)
@@ -35,7 +38,7 @@ f = open('d.pac', 'w')
 f.write(switchy_pac)
 f.close()
 
-switchy_pac = st.substitute(domain=domain_cb)
+v2rayng_pac = st.substitute(domain=domain_cb)
 f = open('d-cb.pac', 'w')
-f.write(switchy_pac)
+f.write(v2rayng_pac)
 f.close()
